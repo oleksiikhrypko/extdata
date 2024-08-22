5fa5980db34e68c0f6af21c576e4ff5fa8815e4d
oleksiikhrypko <alexey.hripko@gmail.com>
2024-08-22 02:47:19 +0200

    initial commit

--- dev-tool/worldlogo/datagetter_test.go
+++ dev-tool/worldlogo/datagetter_test.go
@@ -16,14 +16,16 @@ func Test_getData(t *testing.T) {
 	}
 
 	ctx := context.Background()
-
-	hasMore := true
-	for i := 1; hasMore; i++ {
-		res, err := TakeWorldLogo(ctx, fmt.Sprintf("https://worldvectorlogo.com/alphabetical/a/%d", i))
-		require.NoError(t, err)
-		hasMore = len(res) != 0
-		err = WriteToSCV("./data/worldlogo.csv", res)
-		require.NoError(t, err)
-		<-time.After(2 * time.Second)
+	alphabet := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"}
+	for l := range alphabet {
+		hasMore := true
+		for i := 1; hasMore; i++ {
+			res, err := TakeWorldLogo(ctx, fmt.Sprintf("https://worldvectorlogo.com/alphabetical/%s/%d", l, i))
+			require.NoError(t, err)
+			hasMore = len(res) != 0
+			err = WriteToSCV("./data/worldlogo.csv", res)
+			require.NoError(t, err)
+			<-time.After(2 * time.Second)
+		}
 	}
 }
